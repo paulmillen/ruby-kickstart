fb9bd0a65ced0f3bfbfa3b70b0796a4ce79f685e
paulmillen <pwmillen@gmail.com>
2017-06-08 10:39:51 +0100

    s01c07

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,13 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  to_return = ""
+  string.length.times do |i|
+    to_return << string[i+1] if string[i+1] != nil && (string[i] == "r" || string[i] == "R")
+  end
+  to_return
 end
