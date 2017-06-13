8d72581ed33e7416003704a1a167335ccc2a9f4a
paulmillen <pwmillen@gmail.com>
2017-06-13 17:36:18 +0100

    s03c04

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,19 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str)
+
+  h = {}
+
+   arr = str.split
+
+   arr.each_with_index do |word,index|
+
+   h[word] = arr.index(word)
+
+  end
+
+  h
+
+end
